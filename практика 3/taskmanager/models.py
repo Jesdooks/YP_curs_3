e921bf2f652039bb38180f0af4887d023334000c
Юровский Никита <e.urovskaya@gmail.com>
2025-11-16 20:37:54 +0300

    Класс Task

--- практика 3/taskmanager/models.py
+++ практика 3/taskmanager/models.py
@@ -2,6 +2,33 @@ from datetime import datetime
 
 
 class Task:
+    """
+    Класс Task используется для представления задачи с атрибутами и методами для управления статусом, приоритетом и сроками
+
+    Атрибуты:
+         id (any): уникальный идентификатор задачи
+         title (str): заголовок или краткое описание задачи
+         status (str): статус задачи (по умолчанию 'pending' - в ожидании)
+         priority (str): приоритет задачи (по умолчанию 'normal')
+         created_at (datetime): дата и время создания задачи (если не указано, устанавливается текущая дата и время)
+         due_date (datetime or None): крайний срок выполнения задачи (если есть)
+
+    Методы:
+         to_dict() -> dict:
+             Преобразует объект задачи в словарь с серийными значениями (для сохранения или передачи)
+             Даты преобразуются в ISO формат строк
+
+         from_dict(data: dict) -> Task:
+             Статический метод, создающий объект Task из словаря
+             Преобразует строки с датами из ISO формата в datetime объекты
+             Args:
+                 data (dict): словарь со свойствами задачи, возможно, загруженный из JSON/CSV
+             Returns:
+                 Task: экземпляр задачи с заполненными атрибутами
+             Значения по умолчанию:
+                 status: 'pending'
+                 priority: 'normal'
+    """
     def __init__(self, id, title, status='pending', priority='normal', created_at=None, due_date=None):
         self.id = id
         self.title = title
@@ -11,6 +38,12 @@ class Task:
         self.due_date = due_date
 
     def to_dict(self):
+        """
+        Преобразует объект Task в словарь для сериализации
+
+        Returns:
+            dict: словарь с данными задачи, пригодный для JSON или другого формата
+        """
         return {
             'id': self.id,
             'title': self.title,
@@ -22,6 +55,16 @@ class Task:
 
     @staticmethod
     def from_dict(data):
+        """
+        Создает объект Task из словаря
+
+        Args:
+            data (dict): словарь с ключами и значениями атрибутов задачи,
+                где даты представлены в ISO формате строк.
+
+        Returns:
+            Task: новый объект Task с заполненными атрибутами
+        """
         created_at = datetime.fromisoformat(data['created_at']) if data.get('created_at') else None
         due_date = datetime.fromisoformat(data['due_date']) if data.get('due_date') else None
         return Task(
